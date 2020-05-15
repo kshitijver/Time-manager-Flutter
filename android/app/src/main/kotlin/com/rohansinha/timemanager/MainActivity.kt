61d61686ab73a6c7f1a840673fb6f40fc2ab80da
Rohan Sinha <sinharohan2000@gmail.com>
2020-05-15 10:07:58 +0545

    Initial commit

---
+++ android/app/src/main/kotlin/com/rohansinha/timemanager/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.rohansinha.timemanager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.brown[600],
+        title: Text(
+          'TI.MA',
+
+        ),
+      ),
+    )
+  ));
+}
+
