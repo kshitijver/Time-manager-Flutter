6ac0a42a9ae1bdd52af7038ed6b3088ce8f35591
Rohan Sinha <sinharohan2000@gmail.com>
2020-05-15 16:41:32 +0545

    No change as such

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,43 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.brown[600],
-        title: Text(
-          'TI.MA',
-
-        ),
-      ),
-    )
-  ));
+  runApp(HomePage());
 }
 
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.brown[200],
+      appBar: AppBar(
+        backgroundColor: Colors.brown[600],
+        title: Center(
+          child: Text(
+            'TI.MA',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text('Body'),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.brown,
+      ),
+    ));
+  }
+}
